2e68a3c3f4f429c6213ac542f943727950013c6c
Jake Gibbons <jcg@jayseeg.com>
2017-06-27 01:10:01 -0600

    Multi-address & a refactor cuz why not?

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,12 +2,10 @@ import {combineReducers} from 'redux'
 import {routerReducer} from 'react-router-redux'
 
 import blockchain from './blockchain'
-import wallet from './wallet'
 import blockchainWebsocket from './blockchainWebsocket'
 
 export default combineReducers({
   routing: routerReducer,
   blockchain,
-  wallet,
   blockchainWebsocket,
 })
